3ea48c6a9451d46062b0dd314a17be551ca3b2d5
theBestPatate <jean-pierre.gninahophin@etu.univ-amu.fr>
2025-05-16 21:37:47 +0000

    Buf fix: The notification would keep showing up even when the device was plugged if the battery was below the threshold. It's now fixed and they stop showing up if you plug your laptop

--- battery_alert.fish
+++ battery_alert.fish
@@ -13,8 +13,9 @@ function log_message
     echo (date "+%Y-%m-%d %H:%M:%S") " - $argv" >> $log_file
 end
 
-# Get the current battery percentage
-set battery_percentage (acpi -b | grep -oP '\d+(?=%)')
+# Get the current battery info and store it in lowercase
+set battery_info (string lower (acpi -b))
+set battery_percentage (echo $battery_info | grep -oP '\d+(?=%)')
 
 # Check if the battery percentage was retrieved successfully
 if test -z "$battery_percentage"
@@ -25,23 +26,28 @@ end
 # Log the current battery percentage
 log_message "Current battery percentage: $battery_percentage%"
 
-# Check if the battery percentage is below the threshold
+# Check if the battery percentage is below the threshold and the battery is discharging
 if test $battery_percentage -lt $threshold
-    set full_notification_message "$notification_message: $battery_percentage%"
-    
-    # Attempt to send the notification and capture any errors
-    set dunstify_output (dunstify "$notification_title" "$full_notification_message" -i $icon_path -u critical 2>&1)
-    
-    # Check if dunstify returned an error
-    if test $status -ne 0
-        log_message "dunstify error: $dunstify_output"
-        log_message "dunstify arguments: $notification_title, $full_notification_message, -i $icon_path, -u critical"
+    if string match -q "*discharging*" "$battery_info"
+
+        set full_notification_message "$notification_message: $battery_percentage%"
         
-        # Send a fallback notification
-        set fallback_message "Something went wrong, check the log at: $log_file"
-        dunstify "Error" "$fallback_message" -u critical
-    else
-        log_message "Notification sent: $full_notification_message"
+        # Attempt to send the notification and capture any errors
+        set dunstify_output (dunstify "$notification_title" "$full_notification_message" -i $icon_path -u critical 2>&1)
+        
+        # Check if dunstify returned an error
+        if test $status -ne 0
+            log_message "dunstify error: $dunstify_output"
+            log_message "dunstify arguments: $notification_title, $full_notification_message, -i $icon_path, -u critical"
+            
+            # Send a fallback notification
+            set fallback_message "Something went wrong, check the log at: $log_file"
+            dunstify "Error" "$fallback_message" -u critical
+        else
+            log_message "Notification sent: $full_notification_message"
+        end
+    else 
+        log_message "The laptop is plugged in"
     end
 else
     log_message "Battery percentage is above threshold: $battery_percentage% (above $threshold%)"
